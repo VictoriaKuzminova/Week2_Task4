47625a33493f434a505425d334bb97d3e57fb433
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-05 20:38:40 +0300

    Add files via upload

---
+++ Консольное приложение 4_2/2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void P(int max, int n)
+        {
+            if (n <=max )
+            {
+                for (int i = 1; i<= n; i++)
+                    Console.Write(n + " ");
+                Console.WriteLine();
+                P(max, n + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int max;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите максимальное число последовательности: ");
+                    max = int.Parse(Console.ReadLine());
+                    if (max < 1)
+                    {
+                        Console.WriteLine("Число должно быть положительным");
+                        continue;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод!!!");
+                }
+            }   
+           P(max,n);
+        }
+    }
+}
