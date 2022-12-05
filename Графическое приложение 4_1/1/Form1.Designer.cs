5045b451b22eff5b77594ae169f32dfa9ec5493c
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-05 20:40:42 +0300

    Add files via upload

---
+++ Графическое приложение 4_1/1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.num_textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.res_TextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите натуральное число: ";
+            // 
+            // num_textBox
+            // 
+            this.num_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.num_textBox.Location = new System.Drawing.Point(359, 48);
+            this.num_textBox.Name = "num_textBox";
+            this.num_textBox.Size = new System.Drawing.Size(149, 30);
+            this.num_textBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(227, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вычислить a";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // res_TextBox
+            // 
+            this.res_TextBox.Location = new System.Drawing.Point(55, 168);
+            this.res_TextBox.Name = "res_TextBox";
+            this.res_TextBox.Size = new System.Drawing.Size(453, 96);
+            this.res_TextBox.TabIndex = 3;
+            this.res_TextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 314);
+            this.Controls.Add(this.res_TextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num_textBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Рекурсия";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num_textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox res_TextBox;
+    }
+}
+
---
+++ Графическое приложение 4_1/1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        static int A(int n)
+        {
+            if (n == 1) return 1;
+            else if (n > 1)
+            {
+                int a = n;
+                if ((Math.Pow(2, a - 1) <= n) && (n <= Math.Pow(2, a)))
+                {
+                    return a;
+                }
+                return A(n / 2) + 1;
+            }
+            return 0;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (num_textBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные!!!");
+            }
+            else
+            {
+                try
+                {
+                    n = int.Parse(num_textBox.Text);
+                    if (n == 0)
+                    {
+                        MessageBox.Show("0 - не натуральное число. Попробуйте еще раз!!!");
+                        return;
+                    }
+                    else if (n < 0)
+                    {
+                        MessageBox.Show("Отрицательное число не является натуральным");
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!!!");
+                    return;
+                }
+                res_TextBox.Text = "a = " + A(n);
+
+            }
+        }
+    }
+}
